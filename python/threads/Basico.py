625a05e1c7192d09e4fdd0de5ee4bee1af8d4726
miguelreis14 <30293122+miguelreis14@users.noreply.github.com>
2022-10-18 15:12:37 +0100

    ol

---
+++ python/threads/Basico.py
@@ -0,0 +1,19 @@
+x=2
+y=3
+
+if x>y:
+    print(x,"é maior que",y,"\n")
+else:
+    print(y,"é maior que",x,"\n")
+
+
+for i in range(0,10):
+    print(i)
+print("fim")
+
+
+def tabuada(x):
+    print("tabuada"+str(x))
+
+
+tabuada(2)
