a76051b5ad285747b40fc264c313916d3aa8d4d4
miguelreis14 <30293122+miguelreis14@users.noreply.github.com>
2022-12-20 00:08:06 +0000

    add string detection

--- T_Pratico/client.py
+++ T_Pratico/client.py
@@ -1,17 +1,19 @@
 import xmlrpc.client
+
 # ligacao ao server
 s = xmlrpc.client.ServerProxy('http://localhost:8000')
-
-x=0
-y=0
+# iniciar varieaveis a strings
+x = '0'
+y = '0'
 # Verificacao de valores incorretos e call para o server
-while x == 0 or y == 0:
-    x = int(input("Introduza o primeiro numero "))
-    y = int(input("Introduza o segundo numero "))
-    if (x == 0 or y == 0):
+# funcao isnumeric
+while x == '0' or y == '0' or x.isnumeric() == False or y.isnumeric() == False:
+    x = (input("Introduza o primeiro numero "))
+    y = (input("Introduza o segundo numero "))
+    
+    if x == '0' or '0' == 0 or x.isnumeric() == False or y.isnumeric() == False:
         print("Erro nos valores, tente outra vez")
     else:
-        print(s.isdiv(x, y))
-        break;
-
-
+        # importante fazer cast para int
+        print(s.isdiv(int(x), int(y)))
+        break
